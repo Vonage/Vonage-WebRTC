7a609bf3a1d42786f912ae6bc35c360e395f1319
Pawel Bogdan <pawel.bogdan@vonage.com>
2026-03-09 12:48:35 +0100

    feat: Add SPM support for Vonage-WebRTC 99.4.0-dev.183

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VonageWebRTC_Binary",
-            url: "https://d3opqjmqzxf057.cloudfront.net/vonage-webrtc/pod/vonagewebrtc/release/99.4.0-dev.182/VonageWebRTC-99.4.0-dev.182.zip",
-            checksum: "141bb08d994766eacdcedd2a71050fe1eb14899eeb0603daaedaf158f819000a"
+            url: "https://d3opqjmqzxf057.cloudfront.net/vonage-webrtc/pod/vonagewebrtc/release/99.4.0-dev.183/VonageWebRTC-99.4.0-dev.183.zip",
+            checksum: "347ebd002b6250af6dddedb80bdc58ee602293475a92d108f85c9b3e35532886"
         ),
         .target(
             name: "VonageWebRTC",
